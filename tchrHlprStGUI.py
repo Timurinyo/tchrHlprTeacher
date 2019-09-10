ee5758af97b4e0e3543df7c1af7d85a82e7edb5f
Timurinyo <timurinyo@gmail.com>
2019-09-10 20:35:11 +0300

    removed extra comments

--- tchrHlprStGUI.py
+++ tchrHlprStGUI.py
@@ -52,9 +52,6 @@ class Worker(QRunnable):
         self.kwargs = kwargs
         self.signals = WorkerSignals()
 
-        # Add the callback to our kwargs
-        #self.kwargs['progress_callback'] = self.signals.progress
-
     @pyqtSlot()
     def run(self):
         '''
@@ -89,15 +86,12 @@ class MainWindow(QMainWindow):
         self.studentNames = []
         self.studentAddresses = []
         self.studentAges = []
-        #self.studentInfo = {}
-
-        #self.studentIPs = []
+        
         self.studentCheckboxes = []
         self.studentButtons = []
         self.launchMinecraftPSButtons = []
         self.launchMinecraftPRButtons = []
         self.closeMinecraftButtons = []
-        #self.studentLockStates = []
         self.studentLayouts = []
         self.studentLabelsIcons = []
         self.studentLabels = []
@@ -149,41 +143,13 @@ class MainWindow(QMainWindow):
         splitterLayout.addWidget(sepLine)
         self.layoutV.addLayout(splitterLayout)
 
-
-
-        #file = open('studentsList.txt')
-        #for line in file:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
         self.client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.client.bind(("", 37020))
         self.client.settimeout(False)
 
-        #file.close()
-        #self.screenLocked = False
         self.counter = 0
-        #self.teachermsg = ''
-        #self.createTickBoxsGroup()
-        #layoutStudent = QHBoxLayout()
-        #self.setMinimumSize(QSize(480, 80)) 
-        #layout = QGridLayout()
-        #self.useStylePaletteCheckBox = QCheckBox("&Use style's standard palette")
-        #self.useStylePaletteCheckBox.setChecked(True)
- 
-        #self.l = QLabel("Start")
-        #label = QLabel(self)
-        #pixmap = QPixmap('keep.png')
-        #label.setPixmap(pixmap)
-        #label.setAlignment(Qt.AlignCenter)
-        #layout.addWidget(label)
-        #self.showFullScreen() 
-
-
-        #b = QPushButton("DANGER!")
-        #b.pressed.connect(self.oh_no)
-
-        #layout.addWidget(self.l)
-        #layout.addWidget(b)
-
+        
         w = QWidget()
         w.setLayout(self.layoutV)
 
@@ -194,11 +160,7 @@ class MainWindow(QMainWindow):
         self.threadpool = QThreadPool()
         self.threadpool.setMaxThreadCount(1)
         print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
-
-        #self.IPlistenerThreadpool = QThreadPool()
-        #self.IPlistenerThreadpool.setMaxThreadCount(1)
-
-
+        
         self.timer1 = QTimer()
         self.timer1.setInterval(3000)
         self.timer1.timeout.connect(self.btn_state_timer)
@@ -214,19 +176,6 @@ class MainWindow(QMainWindow):
         self.timer3.timeout.connect(self.olden_students)
         self.timer3.start()
 
-        #self.UDPthreadpool = QThreadPool()
-        #self.UDPthreadpool.setMaxThreadCount(1)
-        #tcpListener = Worker(self.listenTCP)
-        #tcpListener.signals.result.connect(self.do_what_teacher_said)
-        #tcpListener.signals.finished.connect(self.listen_again)
-        ##tcpListener.signals.progress.connect(self.)
-        #self.threadpool.start(tcpListener)
-#
-        #self.hide()
-
-
-    #def createTickBoxsGroup(self):
-    #    self.tickBoxsGroup = QGroupBox("Group 1")
     def launchMinecraftPSEveryChecked(self):
         print("launched minecraft education on all machines")
         for i, cb in enumerate(self.studentCheckboxes):
@@ -245,59 +194,40 @@ class MainWindow(QMainWindow):
             if cb.isChecked():
                 self.closeMinecraftButtons[i].click()
 
-
-
     def make_lockStudent(self, studentIndex):
         def lockStudent():
             if self.studentButtons[studentIndex].isChecked():
                 self.studentButtons[studentIndex].setIcon(QIcon('locked.png'))
                 tcpTeacher = Worker(self.lockMachine, studentIndex)
                 self.threadpool.start(tcpTeacher)
-                #tcpTeacher.signals.result.connect()
-                #tcpListener = Worker(self.listenTCP)
-                #tcpListener.signals.result.connect(self.do_what_teacher_said)
-                #tcpListener.signals.finished.connect(self.listen_again)
-                ##tcpListener.signals.progress.connect(self.)
-                #self.threadpool.start(tcpListener)
                 print("student number {} was locked".format(studentIndex+1))
             elif not self.studentButtons[studentIndex].isChecked():
                 self.studentButtons[studentIndex].setIcon(QIcon('unlocked.png'))
                 tcpTeacher = Worker(self.unlockMachine, studentIndex)
                 self.threadpool.start(tcpTeacher)
-                #tcpListener = Worker(self.listenTCP)
-                #tcpListener.signals.result.connect(self.do_what_teacher_said)
-                #tcpListener.signals.finished.connect(self.listen_again)
-                ##tcpListener.signals.progress.connect(self.)
-                #self.threadpool.start(tcpListener)
                 print("student number {} was unlocked".format(studentIndex+1))
         return lockStudent
 
     def make_launchMinecraftPS(self, studentIndex):
         def launchMinecraftPS():
             self.sendMsgToSt(studentIndex, "launchPS")
-            #print(f"Minecraft launch is not implemented yet for student number {studentIndex}")
         return launchMinecraftPS
 
     def make_launchMinecraftPR(self, studentIndex):
         def launchMinecraftPR():
             self.sendMsgToSt(studentIndex, "launchPR")
-            #print(f"Minecraft launch is not implemented yet for student number {studentIndex}")
         return launchMinecraftPR
 
     def make_closeMinecraft(self, studentIndex):
         def closeMinecraft():
             self.sendMsgToSt(studentIndex, "closeMine")
-            #print(f"Minecraft launch is not implemented yet for student number {studentIndex}")
         return closeMinecraft
 
     def lockMachine(self, studentIndex):
         self.sendMsgToSt(studentIndex, "l")
-        #print("Info from thread: student {} locked".format(studentIndex+1))
-
 
     def unlockMachine(self, studentIndex):
         self.sendMsgToSt(studentIndex, "u")
-        #print("Info from thread: student {} unlocked".format(studentIndex+1))
 
     def sendMsgToSt(self, studentIndex, msg):
         TCP_PORT = 5005
@@ -324,11 +254,6 @@ class MainWindow(QMainWindow):
             return
         s.close()
         print (f"received data from student {studentIndex+1}:", data.decode())
-
-        #if msg == "l":
-        #    self.studentLockStates[studentIndex] = True
-        #elif msg == "u":
-        #    self.studentLockStates[studentIndex] = False
 
     def olden_students(self):
         for i in range(len(self.studentAges)):
@@ -389,37 +314,19 @@ class MainWindow(QMainWindow):
 
 
     def btn_state_timer(self):
-        #studentListener = Worker(self.listenIP)
-        #self.threadpool.start(studentListener)
-        #studentListener = Worker(self.listenIP)
-        #self.IPlistenerThreadpool.start(studentListener)
-        #data, addr = self.client.recvfrom(1024)
-        #print("received message: %s"%data)
-        #self.counter += 1
-        #if self.counter > 10:
         print("sent lock command to those which button state is locked...")
-        #self.counter = 0
-
         for i, btn in enumerate(self.studentButtons):
             if self.studentCheckboxes[i].isChecked(): 
                 if btn.isChecked():
                     self.lockMachine(i)
                 else:
                     self.unlockMachine(i)
-                    #self.studentButtons[i].setChecked(state)
-            #for btn in self.studentButtons:
-            #    if btn.isChecked():
-
-            #self.do_what_teacher_said('u')
-        #self.l.setText("Counter: %d" % self.counter)
-
 
     def checkEverybody(self, state):
         for cb in self.studentCheckboxes:
             cb.setChecked(state)
 
     def lockEveryChecked(self, state):
-        #self.lockAllButton.setChecked(state)
         if self.lockAllButton.isChecked():
             self.lockAllButton.setIcon(QIcon('locked.png'))
             self.lockAllButton.setText('')
@@ -429,7 +336,6 @@ class MainWindow(QMainWindow):
         for i, cb in enumerate(self.studentCheckboxes):
             if cb.isChecked():
                 self.studentButtons[i].setChecked(state)
-        #print("locked all")
 
     def addStudent(self, name, IP):
         print("adding student")
@@ -443,33 +349,22 @@ class MainWindow(QMainWindow):
             self.studentAddresses.append(IP)
             self.studentNames.append(name)
             self.studentAges.append(0)
-            #print("Added name and address")
             self.studentCheckboxes.append(QCheckBox())
             self.studentCheckboxes[-1].setChecked(True)
             self.studentLabels.append(QLabel(name))
             self.studentLabels[-1].setFont(QFont('SansSerif', 10))
-            #print("Added layout")
-            #self.studentLabels[-1]
             self.studentButtons.append(QPushButton(''))
-            #self.studentButtons[-1].setMaximumSize(QSize(32,32))
             self.studentButtons[-1].setCheckable(True)
             self.studentButtons[-1].setChecked(False)
             self.studentButtons[-1].toggled.connect(self.make_lockStudent(self.studentNum-1))
             self.studentButtons[-1].setIcon(QIcon('unlocked.png'))
             self.studentButtons[-1].setIconSize(QSize(100,30))
-            #self.studentLockStates.append(False)
-            #studentLayout = QHBoxLayout()
-
             self.launchMinecraftPSButtons.append(QPushButton(''))
             self.launchMinecraftPSButtons[-1].setChecked(False)
             self.launchMinecraftPSButtons[-1].pressed.connect(self.make_launchMinecraftPS(self.studentNum-1))
             self.launchMinecraftPSButtons[-1].setIcon(QIcon('minecraft.ico'))
             self.launchMinecraftPSButtons[-1].setIconSize(QSize(30,30))
-            self.launchMinecraftPSButtons[-1].hide()
-            '''trying to remmove buttton with hide. If it doesn't help you can test visibility or
-             transperency method and then if still doesn't work just use regular function....It should be even easier!!!
-            '''
-            #other things to improve are in phone notes...Minecraft bug rememmber. Maybe use win32 to setforeground window or something like it... 
+            self.launchMinecraftPSButtons[-1].hide()     
             self.launchMinecraftPRButtons.append(QPushButton(''))
             self.launchMinecraftPRButtons[-1].setChecked(False)
             self.launchMinecraftPRButtons[-1].pressed.connect(self.make_launchMinecraftPR(self.studentNum-1))
@@ -496,38 +391,6 @@ class MainWindow(QMainWindow):
             self.studentLayouts[-1].addWidget(self.closeMinecraftButtons[-1])
             self.studentLayouts[-1].addStretch(1)
             self.layoutV.addLayout(self.studentLayouts[-1])
-
-        #def listenTCP(self, progress_callback):
-        #    TCP_PORT = 5005
-        #    BUFFER_SIZE = 20  # Normally 1024, but we want fast response
-    #
-        #    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #    #timeoutTime = 6
-        #    #s.settimeout(timeoutTime)
-        #    s.bind(('', TCP_PORT)) #TCP_IPs
-        #    s.listen(1)
-        #    try: 
-        #        conn, addr = s.accept()
-        #    except:
-        #        return "Accept error"
-        #    print ('Connection address:', addr)
-        #    while 1:
-        #        data = conn.recv(BUFFER_SIZE)
-        #        if not data: 
-        #            break
-        #        print ("received data:", data.decode())
-        #        teachermsg = data.decode()
-        #        conn.send(data)  # echo                
-        #    conn.close()
-        #    return teachermsg
-
-        #def listen_again(self) :
-        #    tcpListener = Worker(self.listenTCP)
-        #    tcpListener.signals.result.connect(self.do_what_teacher_said)
-        #    tcpListener.signals.finished.connect(self.listen_again)
-        #    #tcpListener.signals.progress.connect(self.)
-        #    self.threadpool.start(tcpListener)
-
 
 app = QApplication([])
 window = MainWindow()
